b0d16c8f666ec5f1c5552c44b3e7f71395ee317b
MIAO KUI <MIAOKUI@MIAOs-MacBook-Pro.local>
2016-03-23 21:33:07 +0800

    update watcher

--- logscan/watch.py
+++ logscan/watch.py
@@ -24,13 +24,22 @@ class watcher(FileSystemEvenHandler):
     def on_modified(self, event):
         self.fd.seek(self.offset, 0)
         for line in self.fd:
-            self.match(self)
+            self.matcher(line)
+        self.offset = self.fd.tell()
 
     def on_moved(self, event):
-
+        if event.src_path == self.path:
+            self.fd.close()
+        if event.dest_path == self.path:
+            self.offset = 0
+            self.fd = open(self.path,'r')
+            self.fd.seek(self.offset, 0)
+            for line in self.fd:
+                self.matcher(line)
+            self.offset = self.fd.tell()
 
     def start(self):
-        self.observer.start()
+        self.observer.schedule(self, self.path, recursive=True)
 
     def stop(self):
         self.observer.join()
